a87c9e3d2224e8d00cc567b834434e04f9ea29d3
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-05-07 08:03:37 -0400

    feat(es8): add padStart and padEnd examples

--- src/es8/index.js
+++ src/es8/index.js
@@ -16,3 +16,24 @@ const entriesCount = Object.entries(data).length;
 let arrayValues = Object.values(data);
 console.log(arrayValues);
 
+
+// Pad a string at the start or end of the string.
+const str1 = '5';
+
+console.log(str1.padStart(3, '0'));
+console.log(str1.padEnd(3, '0'));
+
+
+// Trailing comas
+const obj = {
+  name: 'Oscar',
+}
+
+
+const object1 = {
+  a: 'somestring',
+  b: 42
+};
+
+let b = Object.entries(object1);
+console.log(b);

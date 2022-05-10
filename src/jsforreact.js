877684f72a6d97fb8d64a786f46c55aab681f0ca
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-05-10 09:16:57 -0400

    feat(jsforreact): add object destructuring examples

--- src/jsforreact.js
+++ src/jsforreact.js
@@ -1,3 +1,22 @@
 // arrow functions:
 const functionName = () => "hola";
 console.log(functionName());
+
+// object Destructuring
+const square = {
+  x: 10,
+  y: 10,
+}
+
+function calcArea(square) {
+  const {x,y} = square;
+  return x * y;
+}
+
+// The parameters needs to be the same name of the object properties.
+function calcArea2({x,y}) {
+  return x * y;
+}
+
+console.log(calcArea(square));
+console.log(calcArea2(square));
